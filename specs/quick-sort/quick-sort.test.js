a63c9e99b65813d8b908c086da134053f43463d0
Maggie Saldivia <maggiesaldivia@gmail.com>
2022-06-08 11:42:11 -0400

    add quickSort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -1,9 +1,9 @@
 /*
 
   Quick Sort!
-  
+
   Name your function quickSort.
-  
+
   Quick sort should grab a pivot from the end and then separate the list (not including the pivot)
   into two lists, smaller than the pivot and larger than the pivot. Call quickSort on both of those
   lists independently. Once those two lists come back sorted, concatenate the "left" (or smaller numbers)
@@ -13,12 +13,32 @@
 */
 
 function quickSort(nums) {
-  // code goes here
+    //base case
+    if (nums.length === 0 ||  nums.length === 1) {
+      return nums
+    }
+
+    const pivot = nums[nums.length - 1]
+    const left = []
+    const right = []
+
+    for (let i = 0; i < nums.length - 1; i++) {
+      if (nums[i] < pivot) {
+        left.push(nums[i])
+      } else {
+        right.push(nums[i])
+      }
+    }
+
+    return quickSort(left).concat(pivot, quickSort(right))
+
+
+
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
